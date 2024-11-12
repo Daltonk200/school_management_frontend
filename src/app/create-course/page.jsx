fec9f994b6c1cda119c4cf86f209241d505d0335
Daltonk200 <mbengbright3@gmail.com>
2024-11-12 20:05:22 +0100

    change notfy to toaster

--- src/app/create-course/page.jsx
+++ src/app/create-course/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 // import styles from "./page.module.css";
 // import Sidebar from "@/components/Sidebar/Sidebar";
